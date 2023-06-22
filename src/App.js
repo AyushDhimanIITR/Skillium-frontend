bd1e9b3522bffde478b7c326c57f97f6196fe853
Ayush <ayushd175@gmail.com>
2023-06-22 15:48:18 +0530

    update profile implemented

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Suspense, lazy} from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Loader from "./Components/Loader";
+import GameScreen from "./Components/Dashboard/GameScreen";
 const Navbar = lazy(() => import('./Components/Landing/Navbar/Navbar'));
 const LandingPage = lazy(() => import("./Components/Landing/LandingPage/LandingPage"));
 const Login = lazy(() => import('./Components/Login/Login'));
@@ -21,6 +22,7 @@ function App() {
           </Route>
             <Route path="/profile" element={<Dashboard />} />
             <Route path="/levels" element={<Level />} />
+            <Route path="/gamescreen" element={<GameScreen />} />
             <Route path="/leaderboard" element={<Leaderoard />} />
         </Routes>
       </Suspense>
